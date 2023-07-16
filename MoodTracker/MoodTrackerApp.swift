cb699c1b793e1ab08349607f3426ac2057e62d8f
Forkipe <mhorkiy@gmail.com>
2023-07-16 14:07:02 +0300

    Initial Commit

---
+++ MoodTracker/MoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodTrackerApp.swift
+//  MoodTracker
+//
+//  Created by Марк Горкій on 16.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
